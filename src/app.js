cee24d028bb9ec6cd150fa676f919909b0fc376a
tein1142 <tein1142@hotmail.com>
2021-09-06 01:23:13 +0700

    [UPDATE] Route App

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const subjectRouter = require('./routers/subject')
 const semesterRouter = require('./routers/semester')
 const summarypostRouter = require('./routers/summarypost')
-
+const topicRouter = require('./routers/topic')
+const reviewRouter = require('./routers/review')
 const app = express()
 
 const port = process.env.PORT
@@ -11,6 +12,12 @@ app.use(express.json())
 app.use('/subject',subjectRouter)
 app.use('/semester',semesterRouter)
 app.use('/summarypost',summarypostRouter)
+
+//Tien
+app.use('/topic',topicRouter)
+app.use('/review',reviewRouter)
+
+
 
 app.get('/health',(req,res)=>{
     res.send({status:'This service is healthy.'})
